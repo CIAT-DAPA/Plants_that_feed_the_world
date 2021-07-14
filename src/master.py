55c78b191413b9f6af3dcc6dd2d8c2b9575b4227
stevensotelo <stevenbetancurt@hotmail.com>
2021-07-14 15:46:16 -0500

    downloaded fastat files done

--- src/master.py
+++ src/master.py
@@ -4,18 +4,21 @@
 import os
 import pandas as pd
 
+os.chdir('/indicator/src')
+
 import tools.manage_files as mf
 import tools.download as dl
 
 ##############################################
-# 02 - Setting parameters
+# 02 - Setting configuration
 ##############################################
 
 # Setting global parameters
 root = "/indicator"
-conf_folder = os.path.join(root, "conf")
+data_folder = os.path.join(root, "data")
+conf_folder = os.path.join(data_folder, "conf")
 # Inputs
-inputs_folder = os.path.join(root, "inputs")
+inputs_folder = os.path.join(data_folder, "inputs")
 inputs_f_downloads = os.path.join(inputs_folder, "01_downloads")
 inputs_f_raw = os.path.join(inputs_folder, "02_raw")
 
@@ -24,13 +27,17 @@ mf.mkdir(inputs_folder)
 mf.mkdir(inputs_f_downloads)
 mf.mkdir(inputs_f_raw)
 
-
 # Loading configurations
-conf_downloads = pd.read_excel(os.path.join(conf_folder, "conf.xlsx"), index_col=0, sheet_name='downloads')  
+conf_xls = pd.ExcelFile(os.path.join(conf_folder,"conf.xlsx"))
+conf_downloads = conf_xls.parse("downloads")
+
+# Loading parameters
+countries = pd.read_csv(os.path.join(conf_folder, "countries.csv"), encoding = "UTF-8")
+crops = pd.read_csv(os.path.join(conf_folder, "crops.csv"), encoding = "UTF-8")
 
 ##############################################
-# 03 - Getting data from sources
+# 03 - Downloading data from sources
 ##############################################
 
 # Getting data from faostat
-conf_downloads["status"] = conf_downloads.apply(lambda x: dl.download_url(x.url,os.path.join(inputs_f_downloads,"file")), axis=1)
+conf_downloads["status"] = conf_downloads.apply(lambda x: dl.download_url(x.url,inputs_f_downloads), axis=1)
--- src/tools/download.py
+++ src/tools/download.py
@@ -2,27 +2,44 @@ import os
 import requests
 import zipfile
 import tools.processing_bar as pb
+import tools.manage_files as mf
 
 # Method that download file from url and unzip the file
 # (string) url: Resource's url
 # (string) path: Path where the file should be save
-def download_url(url, path):
+# (bool) force: True if you want to replace the current files. By default it is False
+def download_url(url, path, force = False):
     # Download file
-    with open(path, 'wb') as f:
-        print("\tDownloading: " + url)
-        response = requests.get(url, stream=True)
-        total_length = response.headers.get('content-length')
+    file_name = url.rsplit('/', 1)[-1]
+    full_path = os.path.join(path,file_name)
+    final_path =  os.path.join(path,file_name.rsplit('.', 1)[0])
+    # Check if the file should be remove
+    if force or not os.path.exists(full_path):
+        # remove current files if exist
+        if os.path.exists(full_path):
+            os.remove(full_path)
+            os.remove(final_path)
+        with open(full_path, 'wb') as f:
+            print("\tDownloading: " + url)
+            response = requests.get(url, stream=True)
+            total_length = response.headers.get('content-length')
 
-        if total_length is None: # no content length header
-            f.write(response.content)
-        else:
-            dl = 0
-            total_length = int(total_length)
-            for data in response.iter_content(chunk_size=4096):
-                dl += len(data)
-                f.write(data)
-                pb.progress(total_length, dl)
-    # Unzip process
-    with zipfile.ZipFile(path,"r") as zip_ref:
-        print("\tExtracting: " + path)
-        zip_ref.extractall(path)    
+            if total_length is None: # no content length header
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    pb.progress(total_length, dl)
+        # Unzip process
+        print("\tExtracting: " + full_path)        
+        mf.mkdir(final_path)
+        with zipfile.ZipFile(full_path,"r") as zip_ref:
+            zip_ref.extractall(final_path)
+    
+        return True
+    else:
+        print("\tNot Downloaded: " + url)
+        return False
